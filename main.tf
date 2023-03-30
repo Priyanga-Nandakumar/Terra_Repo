5f19fc0cb566301f938d3dfcb833b1d99f636450
Priyanga Nandakumar <Priyanga.Nandakumar@gds.ey.com>
2023-03-30 18:17:01 +0530

    second commit

--- main.tf
+++ main.tf
@@ -6,13 +6,15 @@ terraform {
     }
   }
 }
+
 provider "aws" {
   # Configuration options
 }
+
 resource "aws_rds_cluster" "default" {
   cluster_identifier      = "aurora-cluster-demo"
   engine                  = "aurora-mysql"
-  engine_version          = "5.7.mysql_aurora.2.03.2"
+  engine_version          = "5.7.mysql_aurora.2.03.2"
   availability_zones      = ["us-west-2a", "us-west-2b", "us-west-2c"]
   database_name           = "mydb"
   master_username         = "foo"
@@ -20,5 +22,3 @@ resource "aws_rds_cluster" "default" {
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
 }
-
-
