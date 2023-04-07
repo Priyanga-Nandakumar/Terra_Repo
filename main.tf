52732ec4b105242c91de18a0c93e07bdbc28a076
Priyanga Nandakumar <Priyanga.Nandakumar@gds.ey.com>
2023-04-07 16:30:33 +0530

    first commit

--- main.tf
+++ main.tf
@@ -18,8 +18,6 @@ resource "aws_glue_security_configuration" "example" {
     s3_encryption {
       aws_kms_key_id = aws_kms_key.test.arn
       s3_encryption_mode = "SSE-KMS"
-      aws_kms_key_id = "Enabled"
-
     }
   }
 }
