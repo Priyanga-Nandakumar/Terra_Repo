6fd25e61e4e5b41d6f057a60343f407a16f06747
Priyanga Nandakumar <Priyanga.Nandakumar@gds.ey.com>
2023-04-07 16:25:23 +0530

    first commit

--- main.tf
+++ main.tf
@@ -11,14 +11,12 @@ provider "aws" {
   # Configuration options
 }
 
-resource "aws_rds_cluster" "default" {
-  cluster_identifier      = "aurora-cluster-demo"
-  engine                  = "aurora-mysql"
-  engine_version          = "5.7.mysql_aurora.2.03.2"
-  availability_zones      = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  database_name           = "mydb"
-  master_username         = "foo"
-  master_password         = "bar"
-  backup_retention_period = 5
-  preferred_backup_window = "07:00-09:00"
-}
+resource "aws_glue_security_configuration" "example" {
+  name = "example"
+
+    s3_encryption {
+      aws_kms_key_id = aws_kms_key.test.arn
+      s3_encryption_mode = "SSE-KMS"
+
+    }
+  }
