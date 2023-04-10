1412163d2936a5d11131737e9b416b36cf80d03c
Priyanga Nandakumar <Priyanga.Nandakumar@gds.ey.com>
2023-04-10 11:11:28 +0530

    first commit

--- main.tf
+++ main.tf
@@ -1,23 +1,18 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.60.0"
+resource "aws_kms_key" "a" {
+  description             = "KMS key 1"
+  deletion_window_in_days = 10
+}
+
+resource "aws_glue_data_catalog_encryption_settings" "example" {
+  data_catalog_encryption_settings {
+    connection_password_encryption {
+      aws_kms_key_id                       = aws_kms_key.test.arn
+      return_connection_password_encrypted = true
     }
-  }
-}
 
-provider "aws" {
-  # Configuration options
-}
-
-resource "aws_glue_security_configuration" "example" {
-  name = "example"
-
-  encryption_configuration {
-    s3_encryption {
-      kms_key_arn = data.aws_kms_key.example.arn
-      s3_encryption_mode = "SSE-KMS"
+    encryption_at_rest {
+      catalog_encryption_mode = "SSE-KMS"
+      sse_aws_kms_key_id      = aws_kms_key.test.arn
     }
   }
 }
